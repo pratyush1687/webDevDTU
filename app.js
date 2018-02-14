056e6bf8badfc4d9cfa98665f1292d4ac061695b
pratyush1687 <pratyush1687@gmail.com>
2018-02-14 13:05:50 +0530

    heh

--- app.js
+++ app.js
@@ -11,13 +11,40 @@ const app = express();
 const train = require('./routes/trainer');
 const user = require('./routes/user')
 const general = require('./routes/general')
+const UserModel = require('./models/user')
 //mongo connect
 mongoose.connect('mongodb://new:password@ds023448.mlab.com:23448/webdevpratyush')
 
 
+app.use(express.static('public'))
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({extended:true}));
+app.use(methodOverride("_method"));
+app.use(flash());
+
+
+
+app.use(require('express-session')({
+    secret: 'dtu hackathon',
+    resave: false,
+    saveUninitialized: false
+}));
+
+app.use(function(req, res, next){
+    res.locals.currentUser = req.user;
+    res.locals.error = req.flash('error');
+    res.locals.success = req.flash('success');
+    next();
+});
+
+app.use(passport.initialize());
+app.use(passport.session());
+passport.use(new LocalStrategy(UserModel.authenticate()));
+passport.serializeUser(UserModel.serializeUser());
+passport.deserializeUser(UserModel.deserializeUser());
+
+
 app.use('/trainer',train);
 app.use('/user',user);
 app.use('/general',general);
